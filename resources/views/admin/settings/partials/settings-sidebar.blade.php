f15ea357c105885e27f0b6bf4768aaf4e2657904
sumit735 <sumitkumardash1999@gmail.com>
2025-10-09 14:58:44 +0530

    Debugbar added

---
+++ resources/views/admin/settings/partials/settings-sidebar.blade.php
@@ -0,0 +1,41 @@
+<div class="col-xl-3 theiaStickySidebar"
+    style="position: relative; overflow: visible; box-sizing: border-box; min-height: 1px;">
+
+    <div class="theiaStickySidebar"
+        style="padding-top: 0px; padding-bottom: 1px; position: static; transform: none; left: 276px; top: 0px;">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex flex-column list-group settings-list">
+                    <a href="bussiness-settings.html"
+                        class="d-inline-flex align-items-center rounded active py-2 px-3"><i
+                            class="ti ti-arrow-badge-right me-2"></i>Business Settings</a>
+                    <a href="seo-settings.html" class="d-inline-flex align-items-center rounded py-2 px-3">SEO
+                        Settings</a>
+                    <a href="localization-settings.html"
+                        class="d-inline-flex align-items-center rounded py-2 px-3">Localization</a>
+                    <a href="prefixes.html" class="d-inline-flex align-items-center rounded py-2 px-3">Prefixes</a>
+                    <a href="preferences.html"
+                        class="d-inline-flex align-items-center rounded py-2 px-3">Preferences</a>
+                    <a href="appearance.html" class="d-inline-flex align-items-center rounded py-2 px-3">Appearance</a>
+                    <a href="language.html" class="d-inline-flex align-items-center rounded py-2 px-3">Language</a>
+                    <a href="authentication-settings.html"
+                        class="d-inline-flex align-items-center rounded py-2 px-3">Authentication Settings</a>
+                    <a href="ai-settings.html" class="d-inline-flex align-items-center rounded py-2 px-3">AI
+                        Settings</a>
+                </div>
+            </div>
+        </div>
+        <div dir="ltr" class="resize-sensor"
+            style="pointer-events: none; position: absolute; inset: 0px; overflow: hidden; z-index: -1; visibility: hidden; max-width: 100%;">
+            <div class="resize-sensor-expand"
+                style="pointer-events: none; position: absolute; inset: 0px; overflow: hidden; z-index: -1; visibility: hidden; max-width: 100%;">
+                <div style="position: absolute; left: 0px; top: 0px; transition: all; width: 396px; height: 410px;">
+                </div>
+            </div>
+            <div class="resize-sensor-shrink"
+                style="pointer-events: none; position: absolute; inset: 0px; overflow: hidden; z-index: -1; visibility: hidden; max-width: 100%;">
+                <div style="position: absolute; left: 0px; top: 0px; transition: all; width: 200%; height: 200%;"></div>
+            </div>
+        </div>
+    </div>
+</div>
